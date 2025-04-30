e8a3de8493be13aea13499c81dc1badac2005ac7
veer-H <haibatpureveer9765@gmail.com>
2025-04-30 11:47:19 +0530

    fix: update sender email address in sendVerificationEmail function

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -17,6 +17,7 @@ export async function GET(request: Request) {
     );
   }
   const userId = new mongoose.Types.ObjectId(_user._id);
+  
   try {
     const user = await UserModel.aggregate([
       { $match: { _id: userId } },
@@ -24,10 +25,11 @@ export async function GET(request: Request) {
       { $sort: { 'messages.createdAt': -1 } },
       { $group: { _id: '$_id', messages: { $push: '$messages' } } },
     ]).exec();
+    
 
-    if (!user || user.length === 0) {
+    if (!user || user.length === 0 || !user[0].messages || user[0].messages.length === 0) {
       return Response.json(
-        { message: 'User not found', success: false },
+        { message: 'User has no messages', success: false },
         { status: 404 }
       );
     }
